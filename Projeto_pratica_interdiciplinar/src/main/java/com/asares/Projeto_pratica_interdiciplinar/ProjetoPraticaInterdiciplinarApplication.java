4eda34817ccd1c2c27c5e70efee876e9a39df784
Dany Stival <danyella.r.s.silva@gmail.com>
2026-09-12 19:42:49 -0300

    primeiro commit

---
+++ Projeto_pratica_interdiciplinar/src/main/java/com/asares/Projeto_pratica_interdiciplinar/ProjetoPraticaInterdiciplinarApplication.java
@@ -0,0 +1,13 @@
+package com.asares.Projeto_pratica_interdiciplinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPraticaInterdiciplinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPraticaInterdiciplinarApplication.class, args);
+	}
+
+}
---
+++ Projeto_pratica_interdiciplinar/src/test/java/com/asares/Projeto_pratica_interdiciplinar/ProjetoPraticaInterdiciplinarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asares.Projeto_pratica_interdiciplinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPraticaInterdiciplinarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
