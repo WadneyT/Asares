454584d93caff6a33379636c41e07928e63bf441
Dany Stival <danyella.r.s.silva@gmail.com>
2026-09-13 21:47:56 -0300

    setup java 21

---
+++ Pratica_Interdisciplinar/src/main/java/com/asares/Pratica_Interdisciplinar/PraticaInterdisciplinarApplication.java
@@ -0,0 +1,13 @@
+package com.asares.Pratica_Interdisciplinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaInterdisciplinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaInterdisciplinarApplication.class, args);
+	}
+
+}
---
+++ Pratica_Interdisciplinar/src/test/java/com/asares/Pratica_Interdisciplinar/PraticaInterdisciplinarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asares.Pratica_Interdisciplinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaInterdisciplinarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
